5af50f3888d060c0f689010c723658ba5d5bab17
msaug <msaug@protonmail.com>
2021-11-16 15:26:53 +0100

    refactored client and threads

--- src/main/java/fr/insalyon/messenger/Main.java
+++ src/main/java/fr/insalyon/messenger/Main.java
@@ -3,7 +3,6 @@ package fr.insalyon.messenger;
 public class Main {
 
     public static void main(String ... args) {
-        System.out.println("Ok");
     }
 
 }
--- src/main/java/fr/insalyon/messenger/net/client/ConnectionHandler.java
+++ src/main/java/fr/insalyon/messenger/net/client/ConnectionHandler.java
@@ -1,7 +1,7 @@
 package fr.insalyon.messenger.net.client;
 
 public interface ConnectionHandler {
-    public void handleConnection(Client client, String serverHost, int serverPort);
+    public void handleConnection(HermesClient client, String serverHost, int serverPort);
 
     public void handleDisconnection();
 
