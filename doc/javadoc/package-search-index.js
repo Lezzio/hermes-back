6ae3b72c1569bf32901ca7b61dbf0874536a64c9
bdeleglise <benoit.deleglise@outlook.fr>
2021-11-26 11:33:35 +0100

    javadoc and .class

---
+++ doc/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"fr.insalyon.messenger"},{"l":"fr.insalyon.messenger.net.model"},{"l":"fr.insalyon.messenger.net.mongodb"},{"l":"fr.insalyon.messenger.net.multicastclient"},{"l":"fr.insalyon.messenger.net.serializer"},{"l":"fr.insalyon.messenger.net.server"},{"l":"fr.insalyon.messenger.stream"}]
